36fe45f21cd8520201f1efd169a2cb4585dbe2c9
Jeronimoelst <108287642+Jeronimoelst@users.noreply.github.com>
2026-09-23 18:33:37 +0200

    operators asg

--- operators_asg.py
+++ operators_asg.py
@@ -17,3 +17,26 @@ Utiliza operadores de asignación abreviados.
 Al final muestra el saldo.
 
 No puedes hacer todas las operaciones mediante una única fórmula.'''
+saldo = 1000
+
+# Recibe 250 €
+saldo += 250
+
+# Gasta 120 €
+saldo -= 120
+
+# Recibe 500 €
+saldo += 500
+
+# Gasta 75 €
+saldo -= 75
+
+# Recibe 300 €
+saldo +=300
+
+print(f"Saldo final: {saldo} €")
+    
+
+
+
+    
